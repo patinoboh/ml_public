51f102227d47677d8711afa0071278502db90c2c
Patrik Brocek <patrik.brocek@gmail.com>
2023-10-17 20:34:00 +0200

    zaloha lebo ori sa zacina angazovat

--- 02/rental_competition_template.py
+++ 02/rental_competition_template.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-#
-#6a81285c-247a-11ec-986f-f39926f24a9c
 
 import argparse
 import lzma
@@ -9,6 +7,13 @@ import pickle
 import sys
 from typing import Optional
 import urllib.request
+import sklearn
+import sklearn.pipeline
+import sklearn.compose
+import sklearn.linear_model
+
+import sklearn.dummy
+
 
 import numpy as np
 import numpy.typing as npt
@@ -54,21 +59,50 @@ class Dataset:
             setattr(self, key, value)
 
 
+def get_integer_columns(matrix):
+    cols = []    
+    for col_num, column in enumerate(matrix.transpose()):
+        integers = True
+        for data in column:
+            integers &= data.is_integer()
+        if(integers):
+            cols.append(col_num)
+    return np.array(cols)
+
 def train_model(data):
-    """
-    nech je to prehladnejsie pls
-    """    
+
 
     # TODO 
     # okay tak model bude vyzerat ze pipeline :
     #   preprocessing : asi teda one hot, scaler a polynomial features
     #                   polynomial features : tie dame do CV skusime ci bude lepsie 1, 2 alebo az 3 ; je mozne ze 1 bude naj lebo hento uz bude overfitted
-    #   potom tam najebeme ten model (a neviem ci sa mozu dat aj viacere aby ich ptm skusal CV)
+    
+    int_columns = get_integer_columns(data.data)
+    
+    model = [("regressor", sklearn.linear_model.LinearRegression())]
+    
+    model = sklearn.pipeline.Pipeline(
+        [sklearn.compose.ColumnTransformer(
+            transformers = [("one hot", sklearn.preprocessing.OneHotEncoder(handle_unknown='ignore', sparse=False), int_columns),
+                            ("scale", sklearn.preprocessing.StandardScaler(), ~int_columns)]
+        ),
+        sklearn.preprocessing.PolynomialFeatures(2, include_bias=False)]
+        + model
+    )
+    
+
+    
+
+
+
+    
+    
+    #   potom tam dame ten model (a neviem ci sa mozu dat aj viacere aby ich ptm skusal CV)
     #   potom vytvorime param_grid ktory bude CV skusat
-    #   a najebeme to donho    
+    #   a dame to donho    
 
 
-    model = ...
+
     return model
 
 
@@ -80,6 +114,10 @@ def main(args: argparse.Namespace) -> Optional[npt.ArrayLike]:
 
         # TODO: Train a model on the given dataset and store it in `model`.
         model = train_model(train)
+
+        # we want to have maximum verbose while training
+        model.verbose = True
+        model.fit(train.data, train.target)
 
         # Serialize the model.
         with lzma.open(args.model_path, "wb") as model_file:
@@ -93,7 +131,7 @@ def main(args: argparse.Namespace) -> Optional[npt.ArrayLike]:
             model = pickle.load(model_file)
 
         # TODO: Generate `predictions` with the test set predictions.
-        predictions = ...
+        predictions = model.predict(test.data)
 
         return predictions
 
@@ -101,3 +139,12 @@ def main(args: argparse.Namespace) -> Optional[npt.ArrayLike]:
 if __name__ == "__main__":
     args = parser.parse_args([] if "__file__" not in globals() else None)
     main(args)
+
+# Rasto Nowak
+#6a81285c-247a-11ec-986f-f39926f24a9c
+
+# Patrik Brocek
+# 5ccdc432-238f-11ec-986f-f39926f24a9c
+
+# Martin Oravec
+# 1056cfa0-24fb-11ec-986f-f39926f24a9c
