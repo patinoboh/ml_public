c2e44c872bfb365d33d7bca61ee5dfbcbf426466
Patrik Brocek <patrik.brocek@gmail.com>
2023-11-13 19:33:57 +0100

    patino first attempt

--- 05/diacritization.py
+++ 05/diacritization.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import argparse
 import lzma
 import os
@@ -6,23 +7,24 @@ import sys
 from typing import Optional
 import urllib.request
 
-import sklearn
-import sklearn.preprocessing
-import sklearn.neural_network
-import sklearn.pipeline
-
 import numpy as np
+import sklearn
+import sklearn.pipeline
+import sklearn.neural_network
+import sklearn.linear_model
 
 parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
-parser.add_argument("--predict", default=True, type=str, help="Path to the dataset to predict")
+# add binary argument
+# parser.add_argument("--pred", default=False, action="store_true", help="Use binary features")
+parser.add_argument("--predict", default=None, type=str, help="Path to the dataset to predict")
 parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
 # For these and any other arguments you add, ReCodEx will keep your default value.
 parser.add_argument("--model_path", default="diacritization.model", type=str, help="Model path")
 
 
-
+# patrik first try
 
 class Dataset:
     LETTERS_NODIA = "acdeeinorstuuyz"
@@ -44,157 +46,106 @@ class Dataset:
         with open(name, "r", encoding="utf-8-sig") as dataset_file:
             self.target = dataset_file.read()
         self.data = self.target.translate(self.DIA_TO_NODIA)
-
-
-def create_model():
-    standard_scaler = sklearn.preprocessing.StandardScaler()
-    algo = sklearn.neural_network.MLPClassifier(hidden_layer_sizes=(40,20))
-
-    cross_valid = sklearn.model_selection.StratifiedKFold(4)
-
-    pipe = sklearn.pipeline.Pipeline(
-        [("scaler",standard_scaler),("algo",algo)]
-    )
-
-    params ={
-        "algo__solver":("lbfgs","sag"),
-        "algo__alpha":[0.00001,0.0004],
-        "algo__learning_rate":["adaptive","invscaling"]
-    }
-
-    return sklearn.model_selection.GridSearchCV(estimator=pipe, cv=cross_valid, param_grid=params, n_jobs=-1, refit=True, verbose=100)
-
-
-def char_to_int(a):
-    if not a.isalpha():
-        return 0
-    a = a.lower()
-    return ord(a) - ord("a") + 1
-
-def kolko_ich_je(data):
-    vsetky_pismenka_ktore_mozu_mat_zmenu = "acdeeinorstuuyzáčďéěíňóřšťúůýž"
-    pocet_zmenitelnych_pismen = 0
-    for pismenko in data:
-        if(pismenko.lower() in vsetky_pismenka_ktore_mozu_mat_zmenu):
-            pocet_zmenitelnych_pismen += 1
-    return pocet_zmenitelnych_pismen
-
-def get_data(data):
-        
-    new_size = kolko_ich_je(data)
-    zajimave = "acdeinorstuyz"
-    # indexes = np.zeros(shape=[new_size])
-    indexes = np.zeros(shape=[new_size]).astype(int)
-
-    win_size = 11
-    pad = int((win_size - 1) / 2)
-    res = np.zeros(shape=[new_size,win_size])
-    window = [0] * win_size
-
-    for i in range(pad + 1):
-        window[pad + i] = char_to_int(data[i])
     
-    row_count = 0
-    
-    for i in range(int(len(data))):
-        if data[i].lower() in zajimave:
-            res[row_count] = np.array(window).reshape(1,-1)
-            indexes[row_count] = i
-            row_count += 1
-            #print(res[i])
-        
-        for j in range(win_size - 1):
-            window[j] = window[j + 1]
-        if pad + i + 1 < len(data):
-            window[-1] = char_to_int(data[pad + i + 1])
-        else:
-            window[-1] = 0
-    
-    return res, indexes
-
-def get_targets(data):
-    # je tam 0 ak nema nic
-    # 1 ak ma dlzen
-    # 2 makcen
-    # 3 kruzok
-    
-    makcene = "řžďčšňěť"
-    dlzne = "áíéýúó"
-    
-    targets = np.zeros((kolko_ich_je(data))) # pre je jeho target ci ma na tom mieste makcen, dlzen, kruzok alebo nic    
-    vsetky_pismenka_ktore_mozu_mat_zmenu = "acdeeinorstuuyzáčďéěíňóřšťúůýž"
-    counter = 0
-        
-    for pismenko in data:    
-        if(pismenko in vsetky_pismenka_ktore_mozu_mat_zmenu):
-            if(pismenko in makcene):
-                targets[counter] = 2 
-            elif(pismenko in dlzne):
-                targets[counter] = 1 
-            elif(pismenko == "ů"):
-                targets[counter] = 3
+    def get_features(self):
+        window_size = 5            # to each side
+        data = self.data.lower()
+        targets = self.target.lower()
+        features, target_features = [], []
+        for i, letter in enumerate(data):            
+            if(letter not in self.LETTERS_NODIA):
+                continue # sorry ale fakt sa to inak nedá napísať            
+            feature = [letter]
+            for offset in list(range(-window_size, 0)) + list(range(1, window_size+1)):
+                if(i+offset < 0 or i + offset >= len(data)):
+                    feature.append(" ") # TODO co tam appendnut, ak sme na kraji?
+                else:
+                    feature.append(data[i-offset])
+            features.append(feature)
+            if targets[i] in "áéíóúý":
+                target_features.append(1)
+            elif targets[i] in "čďěňřšťůž":
+                target_features.append(2)
             else:
-                targets[counter] = 0               
-            counter += 1
-    return targets
+                target_features.append(0)
+            
+        return features, target_features
 
 
+# TODO 
+# 1. neviem ci funguju predictions ale sak to lahko zistime
+# 2. upravit window_size aby sme ju mohli menit len na jednom mieste
+# 3. najebat tam brutal model a eskere
+# 4. dorobit dalsie features - pridat n-gramy
 
-def most_probable_form_of_char(char_to_modify : str, probabilities) -> int:
-    cisto_mekcenove_pismenka = "řžďčšňť"
-    cisto_mekcenove_pismenka_bez_hacku = "rzdcsnt"
-    cisto_dlznove_pismenka = "áíýó"
-    cisto_dlznove_pismenka_bez_carek = "aiyo"
 
-    char = char_to_modify.lower()
-    return_char = char
-    # probabilities[0] -> p0
-    # probabilities[1] -> p1
-    # probabilities[2] -> p2
-    # probabilities[3] -> p3
-    p0 = 1 if probabilities == 0 else 0
-    p1 = 1 if probabilities == 1 else 0
-    p2 = 1 if probabilities == 2 else 0
-    p3 = 1 if probabilities == 3 else 0
-    print(p0, p1, p2, p3)
+def get_predictions(model, data):
+    # window_size musi byt hyperparameter aj pre tuto funkciu
+    # toto sa mi ale vobec nechce
+    # lebo pre prediction toho modelu mu musime poslat ten oneHot list susedov a to je pain
+    # ?? da sa dať na model nejaký .fit_transform?
+    #hmm nepoznam take  
+      
+    window_size = 5
+    predictions = []
+    lower = data.lower()    
     
-    if char in cisto_mekcenove_pismenka_bez_hacku and p2 > p0:
-        return_char = cisto_mekcenove_pismenka[cisto_mekcenove_pismenka_bez_hacku.index(char)]
-    elif char in cisto_dlznove_pismenka_bez_carek and p1 > p0:
-        return_char = cisto_dlznove_pismenka[cisto_dlznove_pismenka_bez_carek.index(char)]
-    elif char == 'u':
-        if p1 > p3 and p1 > p0:
-            return_char = 'ú'
-        if p3 > p1 and p3 > p0:
-            return_char = 'ů'
-    elif char == "e":
-        if p1 > p2 and p1 > p0:
-            return_char = 'é'
-        if p2 > p1 and p2 > p0:
-            return_char = 'ě'
-    else:
-        return "."
-        print("Chyba v metrixu.", end=" ")        
+    lower.append([" "]* window_size)
+    lower.insert(0, [" "]* window_size)    
+    
+    data.append([" "]* window_size)
+    data.insert(0, [" "]* window_size)
+    
+    
 
-    if char_to_modify.isupper():
-        return_char = return_char.upper()
-    return return_char
+    for i in range(window_size, len(data) - window_size):
+        letter = data[i]
+        if(i.lower() not in data.LETTERS_NO_DIA):
+            predictions.append(letter)
+        else:
+            prediction_vector =[lower[i]] + list(lower[i - window_size : i + window_size + 1])
+            # TODO ako upraviť tento vektor na oneHot?                        
+            # TODO pridat na zaciatok PADDING a koniec data s nulovym znakom
+            # ten zac a koniec treba?
+            # hej lebo som s nim nic nerobil kurva fix
+            # idem to spravit to bude ok
 
-def diacritize(input_text : str,
-               operation_indeces : np.ndarray,
-               operations: np.ndarray,
-               predications : np.ndarray) -> str:
-    if operations.shape[0] != operation_indeces.shape[0]:
-        return ''
 
-    list_of_chars = list(input_text)
-    for i in range(operations.shape[0]):
-        swap_index = int(operation_indeces[i])
-        char_to_modify = list_of_chars[swap_index]
-        list_of_chars[swap_index] = most_probable_form_of_char(char_to_modify, predications[i])
+ #         ORI si mysli ze  by tam mal byt len .transform() a mali by sme mat oddelleny transformer a model teda algoritmus ako  
+            prediction_vector = model.predict(prediction_vector) # SNAAAD TODO
+            # co teda este s tymto oneHot
+            # ten bude ok len musim zmenit aby som cyclil cez tie data s offsetom, dumam a nejde mi to pockaj 
+            correction = model.predict(prediction_vector)
+            corrected_letter = ""
+            if correction == 0:
+                predictions.append(letter)
+            elif correction == 1:
+                basic = "aeiouyAEIOUY"
+                dĺžne = "áéíóúýÁÉÍÓÚÝ"
+                corrected_letter = dĺžne[basic.index(letter)]
+            elif correction == 2:
+                normálne_písmen = "cdenrstuzCDENRSTUZ"
+                mäkčene_a_vôkáň = "čďěňřšťůžČĎĚŇŘŠŤuŽ" # neviem napísať veľký vokáň (ak existuje)
+                corrected_letter = mäkčene_a_vôkáň[normálne_písmen.index(letter)]
+            predictions.append(corrected_letter)                    
+    
+    return predictions
 
-    return ''.join(list_of_chars)
 
+def get_model():
+
+
+    # model =sklearn.linear_model.LogisticRegression(verbose = 100, solver = 'saga', max_iter = 100)
+    model = sklearn.neural_network.MLPClassifier(verbose=100,hidden_layer_sizes=(10), max_iter = 10)
+
+    model = sklearn.pipeline.Pipeline([
+            ("scaler", sklearn.preprocessing.OneHotEncoder(handle_unknown='ignore')),
+            ("algo", model),
+        ]) 
+    return model
+    
+    
+    
 
 def main(args: argparse.Namespace) -> Optional[str]:
     if args.predict is None:
@@ -202,11 +153,28 @@ def main(args: argparse.Namespace) -> Optional[str]:
         np.random.seed(args.seed)
         train = Dataset()
 
-        train_data, _ = get_data(train.data)
-        train_targets = get_targets(train.target)
-      
-
-        model = create_model().fit(train_data, train_targets)
+        model = get_model()                
+        features, targets = train.get_features()
+        # split the data into train and target
+        features_train, features_test, targets_train, targets_test = sklearn.model_selection.train_test_split(
+            features, 
+            targets, test_size=1)
+        
+        model.fit(features, targets)
+        # model.fit(features_train, targets_train)        
+        # predictions = model.predict(features_test)
+        
+        # differences = sum(a != b for a, b in zip(predictions, targets_test))
+        # print(differences)    
+        
+        """
+        TOTO ak budeme robit mlp (y) huh
+        
+        model = model.named_steps["algo"]
+        model._optimizer = None
+        for i in range(len(model.coefs_)): model.coefs_[i] = model.coefs_[i].astype(np.float16)
+        for i in range(len(model.intercepts_)): model.intercepts_[i] = model.intercepts_[i].astype(np.float16)
+        """
 
         # Serialize the model.
         with lzma.open(args.model_path, "wb") as model_file:
@@ -214,20 +182,20 @@ def main(args: argparse.Namespace) -> Optional[str]:
 
     else:
         # Use the model and return test set predictions.
-        # test = Dataset(args.predict)
-        test = Dataset()
+        
+        test = Dataset(args.predict)
 
-        data, operation_indices = get_data(test.data)
         with lzma.open(args.model_path, "rb") as model_file:
             model = pickle.load(model_file)
         
-        predictions = model.predict(data)
+        predictions = model.get_predictions(model, test.data)
         
+        return predictions
+
         # TODO: Generate `predictions` with the test set predictions. Specifically,
         # produce a diacritized `str` with exactly the same number of words as `test.data`.
-        predictions = diacritize(test.data, operation_indices, predictions , predictions )
-        print(predictions)
-        return predictions
+        
+        
 
 
 if __name__ == "__main__":
