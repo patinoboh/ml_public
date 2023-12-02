e88a24c6d2c51501398a28703db4fec4ec7faf52
oravecm1 <oravecmartin03@gmail.com>
2023-12-02 17:49:28 +0100

    metric_correlation FINAL

--- 09/metric_correlation.py
+++ 09/metric_correlation.py
@@ -1,14 +1,14 @@
+#!/usr/bin/env python3
 import argparse
 import dataclasses
 
 import numpy as np
 
-
 parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
 parser.add_argument("--bootstrap_samples", default=100, type=int, help="Bootstrap samples")
 parser.add_argument("--data_size", default=1000, type=int, help="Data set size")
-parser.add_argument("--plot", default=True, const=True, nargs="?", type=str, help="Plot the predictions")
+parser.add_argument("--plot", default=False, const=True, nargs="?", type=str, help="Plot the predictions")
 parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
 # For these and any other arguments you add, ReCodEx will keep your default value.
@@ -36,7 +36,7 @@ class ArtificialData:
             self.sentences.append(self.Sentence(gold, predicted, correct, human_rating))
 
 
-def main(args: argparse.Namespace):
+def main(args: argparse.Namespace) -> tuple[float, float]:
     # Create the artificial data.
     data = ArtificialData(args)
 
@@ -51,60 +51,40 @@ def main(args: argparse.Namespace):
         sentences = generator.choice(data.sentences, size=len(data.sentences), replace=True)
 
         # TODO: Append the average of human ratings of `sentences` to `human_ratings`.
-        avg = 0
-        for sentence in sentences:
-            avg += sentence.human_rating
-        avg /= len(sentences)
-        human_ratings.append(avg)
-
+        average_rating = np.mean([sentence.human_rating for sentence in sentences])
+        human_ratings.append(average_rating)
 
         # TODO: Compute TP, FP, FN counts of predicted edits in `sentences`
         # and append them to `predictions`.
-        TP, FP, FN = 0, 0, 0
-        for sentence in sentences:
-            TP += sentence.predicted_correct
-            FP += sentence.predicted_edits - sentence.predicted_correct
-            FN += sentence.gold_edits - sentence.predicted_correct
-            
-        predictions.append((TP, FP, FN))
-
+        tp = sum([sentence.predicted_correct for sentence in sentences])
+        fp = sum([sentence.predicted_edits - sentence.predicted_correct for sentence in sentences])
+        fn = sum([sentence.gold_edits - sentence.predicted_correct for sentence in sentences])
+        predictions.append((tp, fp, fn))
 
     # Compute Pearson correlation between F_beta score and human ratings
     # for betas between 0 and 2.
-
     betas, correlations = [], []
-    i = 0
     for beta in np.linspace(0, 2, 201):
         betas.append(beta)
-
-        x = []
-        x_tilde = 0
-        y_tilde = 0
-
-        for i, avg in enumerate(human_ratings):
-            TP, FP, FN = predictions[i][0], predictions[i][1], predictions[i][2]
-            a = (TP + beta**2 * TP)/(TP + FP + beta**2 * (TP + FN))
-            x_tilde += a
-            y_tilde += human_ratings[i]
-            x.append(a)
-        x_tilde /= len(x)
-        y_tilde /= len(human_ratings)
-
-        citatel = 0
-        menovatel_1 = 0
-        menovatel_2 = 0
-        
-        for i, xi in enumerate(x):
-            citatel += (xi - x_tilde)*(human_ratings[i] - y_tilde)
-            menovatel_1 += (xi - x_tilde) ** 2
-            menovatel_2 += (human_ratings[i] - y_tilde) ** 2
-        r = citatel / ((np.sqrt(menovatel_1)) * (np.sqrt(menovatel_2)))
 
         # TODO: For each bootstrap dataset, compute the F_beta score using
         # the counts in `predictions` and then manually compute the Pearson
         # correlation between the computed scores and `human_ratings`. Append
         # the result to `correlations`.
-        correlations.append(r)
+        f_scores = []
+        for tp, fp, fn in predictions:
+            f_score = (1 + beta**2) * tp / (tp + fp + beta**2 * (tp + fn))
+            f_scores.append(f_score)
+
+        # Compute the correlation manually
+        mean_f_scores = np.mean(f_scores)
+        mean_human_ratings = np.mean(human_ratings)
+        numerator = np.sum((f_scores - mean_f_scores) * (human_ratings - mean_human_ratings))
+        denominator = np.sqrt(np.sum((f_scores - mean_f_scores)**2) * np.sum((human_ratings - mean_human_ratings)**2))
+        correlation = numerator / denominator
+
+        correlations.append(correlation)
+
     if args.plot:
         import matplotlib.pyplot as plt
         plt.plot(betas, correlations)
@@ -114,8 +94,10 @@ def main(args: argparse.Namespace):
 
     # TODO: Assign the highest correlation to `best_correlation` and
     # store corresponding beta to `best_beta`.
-    best_correlation = max(correlations)
-    best_beta = betas[correlations.index(best_correlation)]
+    best_index = np.argmax(correlations)
+    best_beta = betas[best_index]
+    best_correlation = correlations[best_index]
+
     return best_beta, best_correlation
 
 
@@ -126,5 +108,14 @@ if __name__ == "__main__":
     print("Best correlation of {:.3f} was found for beta {:.2f}".format(
         best_correlation, best_beta))
 
-# 5ccdc432-238f-11ec-986f-f39926f24a9c
-# bb29abf7-2547-11ec-986f-f39926f24a9c
+#Rasto Nowak
+
+#6a81285c-247a-11ec-986f-f39926f24a9c
+
+#Patrik Brocek
+
+#5ccdc432-238f-11ec-986f-f39926f24a9c
+
+#Martin Oravec
+
+#1056cfa0-24fb-11ec-986f-f39926f24a9c
