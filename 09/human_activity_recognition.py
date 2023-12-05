5d081e812d5a672d662884fe9a1c25b126a73211
Rastislav Nowak <rasto@Rastos-mac.local>
2023-12-05 20:07:42 +0100

    prvy zeleny bar

--- 09/human_activity_recognition.py
+++ 09/human_activity_recognition.py
@@ -49,30 +49,27 @@ def main(args: argparse.Namespace) -> Optional[npt.ArrayLike]:
 
         # TODO: Train a model on the given dataset and store it in `model`.
         
-        algo = sklearn.ensemble.HistGradientBoostingClassifier(),
-        model = sklearn.pipeline.Pipeline([
-            ("scaler", sklearn.preprocessing.StandardScaler()),
-            ("algo", algo)])
+        model = sklearn.ensemble.HistGradientBoostingClassifier()
+        #model = sklearn.pipeline.Pipeline([
+         #   ("scaler", sklearn.preprocessing.StandardScaler()),
+          #  ("algo", algo)])
         
         # TODO CV
         # este iba max_depth :[4,6,8,10]
-        """grid = {"algo__max_iter": [500,600,700], "algo__max_leaf_nodes":[10,20,30,"None"], "algo__early_stopping":[False, True,"auto"] }
+        grid = {"max_iter": [550,600,625,650], "max_leaf_nodes":[20,30,32], "early_stopping":[False]}
         model = sklearn.model_selection.GridSearchCV(estimator=model,  
                                                       cv = sklearn.model_selection.StratifiedKFold(5), 
                                                       param_grid=grid, 
-                                                      n_jobs=1, 
+                                                      n_jobs=7, 
                                                       refit=True,
-                                                      verbose =100,
-                                                      scoring='accuracy')"""
+                                                      verbose =100)
         
-        scores = sklearn.model_selection.cross_val_score(model, train.data, train.target, cv=sklearn.model_selection.StratifiedKFold(5), n_jobs=7, scoring = 'accuracy')
-        print(args.cv, 100 * scores.mean(), 100 * scores.std())
+
         
         model.fit(train.data, train.target)
         
-        
-        
-        #print(model.best_params_)
+        print("jako",model.best_params_)
+        print("Best Score:", model.best_score_)
         
         # Serialize the model.
         with lzma.open(args.model_path, "wb") as model_file:
@@ -95,3 +92,16 @@ def main(args: argparse.Namespace) -> Optional[npt.ArrayLike]:
 if __name__ == "__main__":
     args = parser.parse_args([] if "__file__" not in globals() else None)
     main(args)
+
+
+#Rasto Nowak
+
+#6a81285c-247a-11ec-986f-f39926f24a9c
+
+#Patrik Brocek
+
+#5ccdc432-238f-11ec-986f-f39926f24a9c
+
+#Martin Oravec
+
+#1056cfa0-24fb-11ec-986f-f39926f24a9c
