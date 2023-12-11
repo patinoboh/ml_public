33fa5e83692a5174f229f92888e2a5b2608f5893
Rastislav Nowak <rasto@Rastos-mac.local>
2023-12-11 22:26:41 +0100

    folder prvyzelenybar

---
+++ 09/prvyzelenybar/human_activity_recognition.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+import argparse
+import lzma
+import pickle
+import os
+import urllib.request
+import sys
+from typing import Optional
+
+import numpy as np
+import numpy.typing as npt
+import pandas as pd
+import sklearn
+import sklearn.ensemble
+import sklearn.pipeline
+
+parser = argparse.ArgumentParser()
+# These arguments will be set appropriately by ReCodEx, even if you change them.
+parser.add_argument("--predict", default=None, type=str, help="Path to the dataset to predict")
+parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
+parser.add_argument("--seed", default=42, type=int, help="Random seed")
+# For these and any other arguments you add, ReCodEx will keep your default value.
+parser.add_argument("--model_path", default="human_activity_recognition.model", type=str, help="Model path")
+
+
+class Dataset:
+    CLASSES = ["sitting", "sittingdown", "standing", "standingup", "walking"]
+
+    def __init__(self,
+                 name="human_activity_recognition.train.csv.xz",
+                 url="https://ufal.mff.cuni.cz/~courses/npfl129/2324/datasets/"):
+        if not os.path.exists(name):
+            print("Downloading dataset {}...".format(name), file=sys.stderr)
+            urllib.request.urlretrieve(url + name, filename="{}.tmp".format(name))
+            os.rename("{}.tmp".format(name), name)
+
+        # Load the dataset and if it contains column "class", split it to `targets`.
+        self.data = pd.read_csv(name)
+        if "class" in self.data:
+            self.target = np.array([Dataset.CLASSES.index(target) for target in self.data["class"]], np.int32)
+            self.data = self.data.drop("class", axis=1)
+
+
+def main(args: argparse.Namespace) -> Optional[npt.ArrayLike]:
+    if args.predict is None:
+        # We are training a model.
+        np.random.seed(args.seed)
+        train = Dataset()
+
+        # TODO: Train a model on the given dataset and store it in `model`.
+        
+        model = sklearn.ensemble.HistGradientBoostingClassifier()
+        #model = sklearn.pipeline.Pipeline([
+         #   ("scaler", sklearn.preprocessing.StandardScaler()),
+          #  ("algo", algo)])
+        
+        # TODO CV
+        # este iba max_depth :[4,6,8,10]
+        grid = {"max_iter": [550,600,625,650], "max_leaf_nodes":[20,30,32], "early_stopping":[False]}
+        model = sklearn.model_selection.GridSearchCV(estimator=model,  
+                                                      cv = sklearn.model_selection.StratifiedKFold(5), 
+                                                      param_grid=grid, 
+                                                      n_jobs=7, 
+                                                      refit=True,
+                                                      verbose =100)
+        
+
+        
+        model.fit(train.data, train.target)
+        
+        print("jako",model.best_params_)
+        print("Best Score:", model.best_score_)
+        
+        # Serialize the model.
+        with lzma.open(args.model_path, "wb") as model_file:
+            pickle.dump(model, model_file)
+
+    else:
+        # Use the model and return test set predictions.
+        test = Dataset(args.predict)
+
+        with lzma.open(args.model_path, "rb") as model_file:
+            model = pickle.load(model_file)
+
+        # TODO: Generate `predictions` with the test set predictions, either
+        # as a Python list or a NumPy array.
+        predictions = model.predict(test.data)
+
+        return predictions
+
+
+if __name__ == "__main__":
+    args = parser.parse_args([] if "__file__" not in globals() else None)
+    main(args)
+
+
+#Rasto Nowak
+
+#6a81285c-247a-11ec-986f-f39926f24a9c
+
+#Patrik Brocek
+
+#5ccdc432-238f-11ec-986f-f39926f24a9c
+
+#Martin Oravec
+
+#1056cfa0-24fb-11ec-986f-f39926f24a9c
