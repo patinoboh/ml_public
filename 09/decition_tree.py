7ee1ccea197bd18eb025114e7f1624e8177423bf
patinoboh <patrik.brocek@gmail.com>
2023-12-18 17:44:47 +0100

    first draft

--- 09/decition_tree.py
+++ 09/decition_tree.py
@@ -59,8 +59,7 @@ class DecisionTree:
         self._data = data
         self._targets = targets
 
-        # change this to something easier to understand
-        self._root = self._leaf(np.arange(len(self._data)))
+        self._root = self._leafify(np.arange(len(self._data))) # node root has all instances
         
         if self._max_leaves is None:
             self._recursive_approach(self._root, 0)
@@ -76,47 +75,49 @@ class DecisionTree:
         if criterion == 0:
             return
         
-        node.split(feature, value, self._leaf(left), self._leaf(right))
-        self._recursive_approach(node.left, depth + 1)
-        self._recursive_approach(node.right, depth + 1)
+        node.split(feature, value, self._leafify(left), self._leafify(right))
+        self._recursive_approach(node.left, depth + 1) # left first
+        self._recursive_approach(node.right, depth + 1) # than right
 
     def _adaptive_approach(self):
+    #   repeatably split a leaf where the
+    #   constraints are valid and the overall criterion value ($c_left + c_right - c_node$)
+    #   decreases the most. If there are several such nodes, choose the one
+    #   which was created sooner (a left child is considered to be created
+    #   before a right child).        
+        def split_value(node, index_in_heap, depth): # criterion_difference, heap_index, depth, node, feature_index, ...
+            best_split = self._best_split(node)
+            return (best_split[0], index_in_heap, depth, node, *best_split[1:])
+                
+        # Function to handle the split of a node
+        def handle_split(node, index_in_heap, depth):
+            if self._can_split(node, depth):
+                heapq.heappush(heap, split_value(node, index_in_heap, depth))
         
-        heap =  []
-        heapq.heappush(heap, ( self._best_split(self._root), 0, 0, self._root, *self._best_split(self._root)))
+        # Initialize heap with root node
+        heap = [split_value(self._root, 0, 0)]
 
+        i = 0
+        # Loop to create splits until maximum number of leaves is reached
+        while heap and i < self._max_leaves - 1:
+            # Pop node with best split from heap
+            _, _, depth, node, feature, value, left, right = heapq.heappop(heap)
 
+            # Perform the split
+            node.split(feature, value, self._leaf(left), self._leaf(right))
 
-    def _find_borders(self, instances, feature):
-        sorted_data_indices = np.argsort(self._data[instances, feature])
-        borders = []
-
-        for i in range(len(sorted_data_indices) - 1):
-            if self._data[sorted_data_indices[i], feature] != self._data[sorted_data_indices[i + 1], feature]:
-                borders.append((self._data[sorted_data_indices[i], feature] + self._data[sorted_data_indices[i + 1], feature]) / 2)    
-        return sorted_data_indices, borders
-    
-    def _best_split(self, node):        
-        # best_crierion, features, value, left, right
-        best_criterion = np.inf
-
-        for feature in range(self._data.shape[1]):
-            sorted_indices, borders = self._find_borders(node.instances, feature)
-            for value in borders:
-                left = sorted_indices[:np.searchsorted(self._data[sorted_indices, feature], value)]
-                right = sorted_indices[np.searchsorted(self._data[sorted_indices, feature], value):]
-                criterion = self._criterion(left) + self._criterion(right)
-            if criterion < best_criterion:
-                best_criterion, best_feature, best_value, best_left, best_right = criterion, feature, value, left, right           
-        
-        return best_criterion - self._criterion(node.instances), best_feature, best_value, best_left, best_right
+            # Handle the split of the child nodes
+            handle_split(node.left, 2 * i + 1, depth + 1)
+            handle_split(node.right, 2 * i + 2, depth + 1)
+            
+            i += 1
 
     def _calculate_criterion(self, sorted_indices, feature, i):
         value = (self._data[sorted_indices[i], feature] + self._data[sorted_indices[i + 1], feature]) / 2
         left, right = sorted_indices[:i + 1], sorted_indices[i + 1:]
         return self._criterion(left) + self._criterion(right), value, left, right
 
-    def _find_best_split(self, node):
+    def _best_split(self, node): # criterion_difference, feature_index, best_value, left, right
         best_criterion = np.inf
         for feature in range(self._data.shape[1]):
             sorted_indices = node.instances[np.argsort(self._data[node.instances, feature])] # TODO check
@@ -131,7 +132,7 @@ class DecisionTree:
 
         return best_criterion - self._criterion(node.instances), best_feature, best_value, best_left, best_right
     
-    def _create_leaf(self, instances):
+    def _leafify(self, instances):
         self._leaves += 1
         classes, counts = np.unique(self._targets[instances], return_counts=True)
         return self.Node(instances, classes[np.argmax(counts)])
@@ -203,8 +204,12 @@ def main(args: argparse.Namespace) -> tuple[float, float]:
     #   which was created sooner (a left child is considered to be created
     #   before a right child).
 
-    # TODO: Finally, measure the training and testing accuracy.
-    train_accuracy, test_accuracy = ...
+    decisiton_tree = DecisionTree(args.criterion, args.max_depth, args.min_to_split, args.max_leaves)
+    decisiton_tree.fit(train_data, train_target)
+
+    # TODO: Finally, measure the training and testing accuracy.    
+    train_accuracy = sklearn.metrics.accuracy_score(train_target, decisiton_tree.predict(train_data))
+    test_accuracy = sklearn.metrics.accuracy_score(test_target, decisiton_tree.predict(test_data))
 
     return 100 * train_accuracy, 100 * test_accuracy
 
@@ -215,3 +220,13 @@ if __name__ == "__main__":
 
     print("Train accuracy: {:.1f}%".format(train_accuracy))
     print("Test accuracy: {:.1f}%".format(test_accuracy))
+
+
+# Patrik Brocek
+# 5ccdc432-238f-11ec-986f-f39926f24a9c
+
+# Rasto Nowak
+# 6a81285c-247a-11ec-986f-f39926f24a9c
+
+# Martin Oravec
+# 1056cfa0-24fb-11ec-986f-f39926f24a9c
